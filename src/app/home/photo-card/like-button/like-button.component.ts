afb9aff95b7cf58f4f363f72872ef4c062fa701c
gkillick <gkillick@gmail.com>
2022-01-09 01:48:27 -0700

    moved detector ref

--- src/app/home/photo-card/like-button/like-button.component.ts
+++ src/app/home/photo-card/like-button/like-button.component.ts
@@ -14,13 +14,13 @@ export class LikeButtonComponent implements OnInit {
   liked = false;
 
   constructor(private likeService: LikeService, private ref: ChangeDetectorRef) {
-    likeService.likedPhotos.subscribe((value)=>{
-      this.liked = this.likeService.isLiked(this.photo);
-      this.ref.detectChanges()
-    })
   }
 
   ngOnInit(): void {
+    this.likeService.likedPhotos.subscribe((value)=>{
+      this.liked = this.likeService.isLiked(this.photo);
+      this.ref.detectChanges()
+    })
     this.liked = this.likeService.isLiked(this.photo)
   }
 
